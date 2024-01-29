1a4e173e72b2b7030fd0f3c93bc56dc21f2a344f
CoderEx24 <coderex24@protonmail.com>
2024-01-30 01:06:47 +0200

    fixed setup vim script

--- scripts/1-setup-vim.sh
+++ scripts/1-setup-vim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim/
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 python install.py --clangd-completer --rust-completer --java-completer
